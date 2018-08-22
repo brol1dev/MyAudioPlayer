ae51ff64549cf7cac4ebc1990a4f0250c7f3617b
Eric Vargas <evargas@ooyala.com>
2018-08-22 15:13:12 -0700

    Create the Xcode project

---
+++ MyAudioPlayer/MyAudioPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAudioPlayer
+//
+//  Created by Eric Vargas on 8/22/18.
+//  Copyright © 2018 Ooyala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
